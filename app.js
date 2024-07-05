50de6f87bff9dc7e7023f8b70ed385441c4e0242
salman <salmannisthar123@gmail.com>
2024-07-05 09:34:28 +0530

    9

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=express()
 const port =3000
 
 app.get("/",(req,res)=>{
-    res.status(200).json({message:'Success. update2..'})
+    res.status(200).json({message:'Success. update3..'})
 })
 
 app.listen(port,()=>{
